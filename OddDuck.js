d40bfab1edf86f76c26274f294cf7aee10b37b89
Ben Halliwell <bkhalliwell@outlook.com>
2023-03-21 21:39:30 +0000

    modified:   OddDuck.css 	modified:   OddDuck.html 	modified:   OddDuck.js

--- OddDuck.js
+++ OddDuck.js
@@ -3,6 +3,7 @@
 const state = [];
 
 let roundsOfVoting = 25;
+let chartObj = null;
 
 function Image(name, source) {
     this.name = name;
@@ -11,25 +12,25 @@ function Image(name, source) {
     this.source = source;
 }
 
-state.push(new Image('bag', 'assets/bag.jpg'));
-state.push(new Image('banana', 'assets/banana.jpg'));
-state.push(new Image('bathroom', 'assets/bathroom.jpg'));
-state.push(new Image('boots', 'assets/boots.jpg'));
-state.push(new Image('breakfast', 'assets/breakfast.jpg'));
-state.push(new Image('bubblegum', 'assets/bubblegum.jpg'));
-state.push(new Image('chair', 'assets/chair.jpg'));
-state.push(new Image('cthulhu', 'assets/cthulhu.jpg'));
-state.push(new Image('dog-duck', 'assets/dog-duck.jpg'));
-state.push(new Image('dragon', 'assets/dragon.jpg'));
-state.push(new Image('pen', 'assets/pen.jpg'));
-state.push(new Image('pet-sweep', 'assets/pet-sweep.jpg'));
-state.push(new Image('scissors', 'assets/scissors.jpg'));
-state.push(new Image('shark', 'assets/shark.jpg'));
-state.push(new Image('sweep', 'assets/sweep.png'));
-state.push(new Image('tauntaun', 'assets/tauntaun.jpg'));
-state.push(new Image('unicorn', 'assets/unicorn.jpg'));
-state.push(new Image('water-can', 'assets/water-can.jpg'));
-state.push(new Image('wine-glass', 'assets/wine-glass.jpg'));
+state.push(new Image('Bag', 'assets/bag.jpg'));
+state.push(new Image('Banana', 'assets/banana.jpg'));
+state.push(new Image('Bathroom', 'assets/bathroom.jpg'));
+state.push(new Image('Boots', 'assets/boots.jpg'));
+state.push(new Image('Breakfast', 'assets/breakfast.jpg'));
+state.push(new Image('Bubblegum', 'assets/bubblegum.jpg'));
+state.push(new Image('Chair', 'assets/chair.jpg'));
+state.push(new Image('Cthulhu', 'assets/cthulhu.jpg'));
+state.push(new Image('Dog-duck', 'assets/dog-duck.jpg'));
+state.push(new Image('Dragon', 'assets/dragon.jpg'));
+state.push(new Image('Pen', 'assets/pen.jpg'));
+state.push(new Image('Pet-sweep', 'assets/pet-sweep.jpg'));
+state.push(new Image('Scissors', 'assets/scissors.jpg'));
+state.push(new Image('Shark', 'assets/shark.jpg'));
+state.push(new Image('Sweep', 'assets/sweep.png'));
+state.push(new Image('Tauntaun', 'assets/tauntaun.jpg'));
+state.push(new Image('Unicorn', 'assets/unicorn.jpg'));
+state.push(new Image('Water-can', 'assets/water-can.jpg'));
+state.push(new Image('Wine-glass', 'assets/wine-glass.jpg'));
 
 let imgEls = document.querySelectorAll('img');
 let voteTrackerEl = document.getElementById('vote-tracker');
@@ -41,21 +42,27 @@ console.log('CURRENTLY RENDERED IMAGES', imgEls);
 function generateRandomProduct() {
     return Math.floor(Math.random() * state.length);
 }
+let uniqueIndexValues = [];
 
+// loading up a line of 6 indexed spots; pulling the first three, and setting the next three 'on deck'
 function renderProducts() {
-    let product1 = state[generateRandomProduct()];
-    let product2 = state[generateRandomProduct()];
-    let product3 = state[generateRandomProduct()];
-    console.log('PRODUCTS to re-render', imgEls, product1, product2);
-    while (product1.name === product2.name) {
-        product2 = state[generateRandomProduct()];
-    };
-    while (product2.name === product3.name) {
-        product3 = state[generateRandomProduct()];
-    };
-    while (product1.name === product3.name) {
-        product3 = state[generateRandomProduct()];
-    };
+    while (uniqueIndexValues.length < 6) {
+        let index = generateRandomProduct();
+        while (uniqueIndexValues.includes(index)) {
+            index = generateRandomProduct();
+        }
+        uniqueIndexValues.push(index);
+    }
+    console.log(uniqueIndexValues);
+
+    let product1 = state[uniqueIndexValues[0]];
+    let product2 = state[uniqueIndexValues[1]];
+    let product3 = state[uniqueIndexValues[2]];
+
+    uniqueIndexValues.shift();
+    uniqueIndexValues.shift();
+    uniqueIndexValues.shift();
+    console.log(uniqueIndexValues);
 
     imgEls[0].src = product1.source;
     imgEls[0].id = product1.name;
@@ -69,7 +76,7 @@ function renderProducts() {
 }
 
 function handleProductClick(event) {
-    console.log(event.target);
+    // console.log(event.target);
 
     let productThatWasClicked = event.target.id;
     state.forEach(image => {
@@ -83,16 +90,138 @@ function handleProductClick(event) {
         roundsOfVoting--;
     } else {
         voteTrackerEl.removeEventListener('click', handleProductClick);
-        const button = document.createElement('button');
-        button.innerText = 'Results!';
-        button.id = 'mainButton';
-        button.addEventListener('click', () => {
-            document.write(this.timesClicked, this.timesShown);
-        })
-        document.body.appendChild(button)
-    }
+        chartObj = drawChart();
+        let buttonEl = document.getElementById('results-button');
+        buttonEl.addEventListener('click', renderData);
+        alert('No more votes available!');
+    };
 };
+
 
 voteTrackerEl.addEventListener('click', handleProductClick);
 
 renderProducts();
+
+function renderData(event) {
+    // let buttonClicked = event.target.id;
+    state.forEach(image =>{
+        let listItemEl = document.createElement('li');
+        let parentContainer = document.getElementById('results-list');
+        parentContainer.appendChild(listItemEl);
+        listItemEl.innerHTML = `Product: ${image.name} had ${image.timesClicked} votes and was shown ${image.timesShown} times.`;
+        image.timesClicked;
+        image.timesShown;
+    });
+};
+
+const canvasEl = document.getElementById('myChart');
+
+function drawChart() {
+    let labels = []
+    let timesShownValues = [];
+    let timesClickedValues = []
+    state.forEach(image => {
+    
+        labels.push(image.name);
+        timesShownValues.push(image.timesShown);
+        timesClickedValues.push(image.timesClicked);
+    });
+
+return new Chart(canvasEl, {
+  type: 'bar',
+  data: {
+    labels: labels,
+    datasets: [{ 
+      label: 'Total # of Votes',
+      data: timesClickedValues,
+      borderWidth: 1
+    }, {
+    label: 'Total # of times shown',
+    data: timesShownValues,
+    borderWidth: 1
+  }],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
+};
+
+// const canvasEl = document.getElementById('canvas');
+
+// const drawingContext = canvasEl.getContext('2d');
+
+// drawingContext.strokeRect(0, 0, 10, 10);
+// drawingContext.strokeRect(10, 10, 10, 10);
+// drawingContext.strokeRect(20, 20, 10, 10);
+// drawingContext.strokeRect(30, 30, 10, 10);
+// drawingContext.strokeRect(40, 40, 10, 10);
+// drawingContext.strokeRect(50, 50, 10, 10);
+// drawingContext.strokeRect(60, 60, 10, 10);
+// drawingContext.strokeRect(70, 70, 10, 10);
+// drawingContext.strokeRect(80, 80, 10, 10);
+// drawingContext.strokeRect(90, 90, 10, 10);
+// drawingContext.strokeRect(100, 100, 10, 10);
+// drawingContext.strokeRect(110, 110, 10, 10);
+// drawingContext.strokeRect(120, 120, 10, 10);
+// drawingContext.strokeRect(130, 130, 10, 10);
+// drawingContext.strokeRect(140, 140, 10, 10);
+// drawingContext.strokeRect(150, 150, 10, 10);
+// drawingContext.strokeRect(160, 160, 10, 10);
+// drawingContext.strokeRect(170, 170, 10, 10);
+// drawingContext.strokeRect(180, 180, 10, 10);
+// drawingContext.strokeRect(190, 190, 10, 10);
+// drawingContext.strokeRect(200, 200, 10, 10);
+// drawingContext.strokeRect(210, 210, 10, 10);
+// drawingContext.strokeRect(220, 220, 10, 10);
+// drawingContext.strokeRect(230, 230, 10, 10);
+// drawingContext.strokeRect(240, 240, 10, 10);
+// drawingContext.strokeRect(250, 250, 10, 10);
+
+// drawingContext.strokeRect(490, 0, 10, 10);
+// drawingContext.strokeRect(480, 10, 10, 10);
+// drawingContext.strokeRect(470, 20, 10, 10);
+// drawingContext.strokeRect(460, 30, 10, 10);
+// drawingContext.strokeRect(450, 40, 10, 10);
+// drawingContext.strokeRect(440, 50, 10, 10);
+// drawingContext.strokeRect(430, 60, 10, 10);
+// drawingContext.strokeRect(420, 70, 10, 10);
+// drawingContext.strokeRect(410, 80, 10, 10);
+// drawingContext.strokeRect(400, 90, 10, 10);
+// drawingContext.strokeRect(390, 100, 10, 10);
+// drawingContext.strokeRect(380, 110, 10, 10);
+// drawingContext.strokeRect(370, 120, 10, 10);
+// drawingContext.strokeRect(360, 130, 10, 10);
+// drawingContext.strokeRect(350, 140, 10, 10);
+// drawingContext.strokeRect(340, 150, 10, 10);
+// drawingContext.strokeRect(330, 160, 10, 10);
+// drawingContext.strokeRect(320, 170, 10, 10);
+// drawingContext.strokeRect(310, 180, 10, 10);
+// drawingContext.strokeRect(300, 190, 10, 10);
+// drawingContext.strokeRect(290, 200, 10, 10);
+// drawingContext.strokeRect(280, 210, 10, 10);
+// drawingContext.strokeRect(270, 220, 10, 10);
+// drawingContext.strokeRect(260, 230, 10, 10);
+// drawingContext.strokeRect(250, 240, 10, 10);
+// drawingContext.strokeRect(240, 250, 10, 10);
+
+
+
+// let scale = 1;
+// function drawRectangles() {
+//     let x = 0;
+//     let y = 0;
+//     let height = 10 * scale;
+//     let width = 10 * scale;
+//     drawingContext.strokeRect(x, y, height, width);
+// }
+
+// setInterval(() => {
+//     drawRectangles(scale += 1); 
+// }, 500);
+
+// console('THIS IS THE CHART LIBRARY', );
